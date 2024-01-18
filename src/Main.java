ad466bb6db56bd239fe4952748ae4e0a57e71fb9
Chante Stewart-Wallace <47185829+stewartwalc1@users.noreply.github.com>
2024-01-18 18:44:42 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+        int num1, num2;
+
+        Scanner scan = new Scanner(System.in);
+
+        num1 = scan.nextInt();
+        num2 = scan.nextInt();
+
+        System.out.println(num1 + num2);
+    }
+}
